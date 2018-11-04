d1b2697c930b19cbf7157f9c2479b13bab641584
Steven Cheung <skwcheung@v1020-wn-187-81.campus-dynamic.uwaterloo.ca>
2018-11-04 13:45:12 -0500

    Created produce_values and push_to_queue func for producer. WIP (Need to test if compiles)
    Fixed compile errors from prev commit
    
    Fixed compile errors
    
    Created produce_values and push_to_queue functions for Producer. Compiles but needs to be tested

--- src/processes/produce.c
+++ src/processes/produce.c
@@ -15,7 +15,39 @@
 #include <math.h>
 
 double g_time[2];
+int MAX_SIZE = 10;
+char  *QUEUE_NAME = "/queue";
 
+/* Function to write message to pipe */
+int push_to_queue(int value, mqd_t mq){
+	if(mq_send(mq, (char *) &value, sizeof(value),0) == -1){
+		perror("Error: Send message failed");
+	}
+	printf("Produced value %d to queue",value);
+}
+
+/* Function producers will call to determine values to push*/
+int produce_values(int id,int num_producers,int input_array[],int size, mqd_t mq,char* qname){
+	/* Open message queue at beginning of produce_values */
+	mq  = mq_open(qname, O_WRONLY);
+	int itr;
+	if (mq == -1 ) {
+		perror("mq_open() failed");
+		exit(1);
+	}
+
+	for(itr=0;itr < size;itr++){
+		if(itr%num_producers==id){
+			push_to_queue(itr,mq);
+		}
+	}
+
+	/* cleanup */
+    if(mq_close(mq) == -1){
+	exit(1);
+	printf("Error: Could not close queue");
+    }	
+}
 
 int main(int argc, char *argv[])
 {
@@ -25,6 +57,24 @@ int main(int argc, char *argv[])
 	int num_c;
 	int i;
 	struct timeval tv;
+	mqd_t mq;
+	mode_t mode = S_IRUSR | S_IWUSR;
+    struct mq_attr attr;
+    char buffer[MAX_SIZE + 1];
+
+    /* initialize the queue attributes */
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = 100;
+    attr.mq_msgsize = sizeof(int);
+    attr.mq_curmsgs = 0;
+
+    /* create the message queue */
+    mq = mq_open(QUEUE_NAME, O_CREAT | O_RDONLY, mode, &attr);
+	if (mq == -1 ) {
+		perror("mq_open() failed");
+		exit(1);
+	}
+
 
 	if (argc != 5) {
 		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
