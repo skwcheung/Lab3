737f2db6969adfb8ba183d402c77dba1e04335d5
Steven Cheung <skwcheung@v1020-wn-187-81.campus-dynamic.uwaterloo.ca>
2018-11-11 18:00:57 -0500

    Created all functions. Compiles but may not work need to test

--- src/processes/produce.c
+++ src/processes/produce.c
@@ -13,10 +13,39 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <math.h>
+#include <errno.h>
 
 double g_time[2];
 int MAX_SIZE = 10;
-char  *QUEUE_NAME = "/queue";
+
+int check_sqrt(int value){
+	double result;
+	result = sqrt(value);
+	if(result == value)
+		return value;
+	else	
+		return 0;
+}
+
+/* Function to read messages from queue */
+int recieve_from_queue(mqd_t mq,char* qname){
+	mq  = mq_open(qname, O_RDONLY);
+	if (mq == -1 ) {
+		perror("mq_open()");
+		exit(1);
+	}
+
+	int value;
+	/* only block for a limited time if the queue is empty */
+	if (mq_receive(mq, (char *) &value, sizeof(int), 0) == -1) {
+		perror("\nmq_receive() failed");
+
+		return -1;
+	} else {
+		printf("Recieved value %d",value);
+		return value;
+	}
+}
 
 /* Function to write message to pipe */
 int push_to_queue(int value, mqd_t mq){
@@ -27,7 +56,7 @@ int push_to_queue(int value, mqd_t mq){
 }
 
 /* Function producers will call to determine values to push*/
-int produce_values(int id,int num_producers,int input_array[],int size, mqd_t mq,char* qname){
+int produce_values(int id,int num_producers,int size, mqd_t mq,char* qname){
 	/* Open message queue at beginning of produce_values */
 	mq  = mq_open(qname, O_WRONLY);
 	int itr;
@@ -49,32 +78,75 @@ int produce_values(int id,int num_producers,int input_array[],int size, mqd_t mq
     }	
 }
 
-int main(int argc, char *argv[])
-{
+int consume_values(int id,mqd_t mq, char* qname){
+	int value;
+	value = recieve_from_queue(mq,qname);
+
+	int root = check_sqrt(value);
+	if(root){
+		printf("&d &d &d",id,value,root);
+	}
+
+	if (mq_close(mq) == -1) {
+		perror("mq_close() failed");
+		exit(2);
+	}
+
+}
+
+/* Loop through and fork num_p times to create num_p producer processes */
+int create_producers(int num_of_producers,int num,mqd_t mq,char* qname){
+	int id;
+	for(int producer = 0;producer < num_of_producers;producer++){
+		id = producer;
+		pid_t pid;
+
+		pid = fork();
+		if(pid < 0){
+			perror("fork failed");
+		}
+		else if(pid == 0){
+			/* Child process */
+			produce_values(id,num_of_producers,num,mq,qname);
+		}
+		else{
+			/* Parent process */
+		}
+	}
+}
+
+int create_consumers(int num_of_consumers, mqd_t mq, char* qname){
+	int id;
+	for(int consumer = 0;consumer < num_of_consumers;consumer++){
+		id = consumer;
+		pid_t pid;
+
+		pid = fork();
+		if(pid < 0){
+			perror("fork failed");
+		}
+		else if(pid == 0){
+			/* Child process */
+			consume_values(id,mq,qname);
+		}
+		else{
+			/* Parent process */
+		}
+	}
+}
+
+int main(int argc, char *argv[]){
 	int num;
 	int maxmsg;
 	int num_p;
 	int num_c;
 	int i;
 	struct timeval tv;
+	char *qname = "/mailbox1_skwcheun";
 	mqd_t mq;
 	mode_t mode = S_IRUSR | S_IWUSR;
     struct mq_attr attr;
     char buffer[MAX_SIZE + 1];
-
-    /* initialize the queue attributes */
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = 100;
-    attr.mq_msgsize = sizeof(int);
-    attr.mq_curmsgs = 0;
-
-    /* create the message queue */
-    mq = mq_open(QUEUE_NAME, O_CREAT | O_RDONLY, mode, &attr);
-	if (mq == -1 ) {
-		perror("mq_open() failed");
-		exit(1);
-	}
-
 
 	if (argc != 5) {
 		printf("Usage: %s <N> <B> <P> <C>\n", argv[0]);
@@ -86,10 +158,26 @@ int main(int argc, char *argv[])
 	num_p = atoi(argv[3]);  /* number of producers        */
 	num_c = atoi(argv[4]);  /* number of consumers        */
 
+	int values[num];
+
+	/* initialize the queue attributes */
+    attr.mq_flags = 0;
+    attr.mq_maxmsg = maxmsg;
+    attr.mq_msgsize = sizeof(int);
 
 	gettimeofday(&tv, NULL);
 	g_time[0] = (tv.tv_sec) + tv.tv_usec/1000000.;
 
+	/* create the message queue */
+	printf("Attempting mq_open first time");
+    mq = mq_open(qname, O_RDWR | O_CREAT , mode, &attr);
+	if (mq == -1 ) {
+		perror("mq_open() failed");
+		exit(1);
+	}
+
+	create_producers(num_p,num,mq,qname);
+	create_consumers(num_c,mq,qname);
 
     gettimeofday(&tv, NULL);
     g_time[1] = (tv.tv_sec) + tv.tv_usec/1000000.;
@@ -98,3 +186,4 @@ int main(int argc, char *argv[])
             g_time[1] - g_time[0]);
 	exit(0);
 }
+
